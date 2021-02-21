cb8b790eb4e607672978a17445f44c855e92daa2
Lars Söderman <lars.soderman@gmail.com>
2021-02-21 23:43:33 +0100

    refactored a lot of functions and switched to one call for the weather api

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 
 
 https://kitten-weather.netlify.app/
+
+
+
+
+### TODO
+- separate render page elements
+- add my searches
+- add menu
+
+
+geo loc OR search and get coords
+
+get one call weather from coords
+
+get city from coords
+
+render city in head
+
+render weather and forecast
+
--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const main = document.getElementById('main')
+// const content = document.getElementById('content')
 const date = document.getElementById('date')
-const timeContainer = document.getElementById('time-container')
-// const jsArea = document.getElementById('js-area')
-const currentWeatherContainer = document.getElementById('current-weather-container')
-const sunriseSunsetContainer = document.getElementById('sunrise-sunset-container')
-const dayForecastContainer = document.getElementById('day-forecast-container')
-const weekForecastContainer = document.getElementById('week-forecast-container')
+// const timePlace = document.getElementById('time-place')
+const time = document.getElementById('time')
+const place = document.getElementById('place')
+const weatherContainer = document.getElementById('weather-container')
+// const currentWeather = document.getElementById('current-weather')
+// const sunriseSunset = document.getElementById('sunrise-sunset')
+// const dayForecastC = document.getElementById('day-forecast')
+// const weekForecast = document.getElementById('week-forecast')
 const cityInput = document.getElementById('city-input')
-
 const geoLocate = document.getElementById('geo-locate')
 const searchBtn = document.getElementById('search-btn')
 
-// let city = ''
-// let country = ''
+
 const locationIQAccessToken = 'pk.cb782d57e1da5eb2b2f70566b4fdb2eb'
 const openWeatherApiKey = '3addfde144e16d817dcc3a5e9a46ea59' 
-let latitude = ''
-let longitude = ''
+// let latitude = ''
+// let longitude = ''
+let foundPlace
+
+
+// Put date in nav
+date.innerHTML = new Date().toLocaleString('en-EN', { weekday: 'long', month: 'long', day: 'numeric' })
 
 const dateToday = () => {
     return new Date().toLocaleDateString()
@@ -33,127 +39,93 @@ const getWeekday = (daysFromToday) => {
     return week[now.getDay() + daysFromToday]
 }
 
-const renderCurrentWeather = (json) => {
-    currentWeatherContainer.innerHTML =`
-        <h3>${json.weather[0].main}</h3>
-        <img src='https://openweathermap.org/img/wn/${json.weather[0].icon}@4x.png'>
-        <h2>${json.main.temp.toFixed()}°</h2>`
-    sunriseSunsetContainer.innerHTML = `
+const renderWeather = (json) => {
+    const renderDayForecast = (json) => {
+        let dayForecast = ''
+        for(let i = 0; i < 7; i++) {
+            dayForecast +=
+            `<div class="line ${(new Date(json.hourly[i].dt * 1000).toDateString() === new Date().toDateString()) ? `today` : `not-today`}">
+                <p>${toTimeString(json.hourly[i].dt)}</p>
+                <img src="https://openweathermap.org/img/wn/${json.hourly[i].weather[0].icon}@2x.png">
+                <p class="forecast-temperature">${Math.round(json.hourly[i].temp)}°</p>
+            </div>`   
+        }
+        return dayForecast
+    }
+    const renderWeekForecast = (json) => {
+        let weekForecast = ''
+        for(let i = 0; i < 7; i++) {
+            weekForecast +=
+            `<div class="line">
+                <p>${getWeekday(i + 1)}</p>
+                <img src="https://openweathermap.org/img/wn/${json.daily[i].weather[0].icon}@2x.png">
+                <p class="forecast-temperature">${Math.round(json.daily[i].temp.day)}°</p>
+            </div>`
+        }
+        return weekForecast
+    }
+    time.innerHTML = `<p>${new Date().toLocaleTimeString(['se-SE'], { hour: '2-digit', minute: '2-digit' })}</p>`
+    weatherContainer.innerHTML =`
+
+    <section id="current-weather">
+        <h3>${json.current.weather[0].main}</h3>
+        <img src='https://openweathermap.org/img/wn/${json.current.weather[0].icon}@4x.png'>
+        <h2>${json.current.temp.toFixed()}°</h2>
+    </section>
+    <section id="sunrise-sunset">
         <div>
             <img src='./icons/sunrise.svg'>
-            <h4>${toTimeString(json.sys.sunrise)}</h4>
+            <h4>${toTimeString(json.current.sunrise)}</h4>
         </div>
         <div>
             <img src='./icons/sunset.svg'>
-            <h4>${toTimeString(json.sys.sunset)}</h4>
-        </div>`
+            <h4>${toTimeString(json.current.sunset)}</h4>
+        </div>
+    </section>
+    <div class="divider"></div>
+    <section id="day-forecast" class="day forecast">
+        ${renderDayForecast(json)}
+    </section>
+    <div class="divider"></div>
+    <section id="week-forecast" class="week forecast">
+    ${renderWeekForecast(json)}
+    </section>
+    <div class="divider"></div>`
+    
 }
 
-const renderForecast = (json) => {
-    // Output forecast for current day
-    for(let i = 0; i < 6; i++) {
-        dayForecastContainer.innerHTML += `
-        <div class="line ${json.list[i].dt_txt.includes(dateToday()) ? `today` : `not-today`}">
-        <p>${toTimeString(json.list[i].dt)}</p>
-                <img src="https://openweathermap.org/img/wn/${json.list[i].weather[0].icon}@2x.png">
-                <p class="forecast-temperature">${Math.round(json.list[i].main.temp_min)}° / ${Math.round(json.list[i].main.temp_max)}°</p>
-                </div>`
-    }
-    const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00'))
-    for(let i = 0; i < filteredForecast.length; i++) {
-        weekForecastContainer.innerHTML += `
-        <div class="line">
-        <p>${getWeekday(i + 1)}</p>
-                <img src="https://openweathermap.org/img/wn/${filteredForecast[i].weather[0].icon}@2x.png">
-                <p class="forecast-temperature">${Math.round(filteredForecast[i].main.temp)}°</p>
-            </div>`
-    }
-}
-
-// Put date in nav
-date.innerHTML = new Date().toLocaleString('en-EN', { weekday: 'long', month: 'long', day: 'numeric' })
-// Put time current weather container
-timeContainer.innerHTML = new Date().toLocaleTimeString(['se-SE'], { hour: '2-digit', minute: '2-digit' })
-
-// Get current weather from city and country
-const getCurrentWeather = () => {
-    if (city && country) {
-        const currentWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&APPID=`
-        console.log('')
-        fetch (currentWeatherApiUrl + openWeatherApiKey)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            renderCurrentWeather(json)
-        })
-    } else {
-        console.log('error - no city and country data')
-    }
-}
-
-// Get forecast from coordinates    
-const getForecastFromCoords = (lat, lon) => {
-    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=`
-    fetch (forecastApiUrl + openWeatherApiKey)
-    .then ((response) => {
-        return response.json()
-    })
-    .then ((json) => {
-        console.log(json)
-        renderForecast(json)
-    })
-    .catch ((err) => {
-        console.log('caught error', err)
-        dayForecastContainer.innerHTML = `caught error: ${err}`
-    }) 
-}
-// Get forecast from city and country    
-const getForecast = () => {
-    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=metric&APPID=`
-    fetch (forecastApiUrl + openWeatherApiKey)
-    .then ((response) => {
-        return response.json()
-    })
-    .then ((json) => {
-        console.log(json)
-        renderForecast(json)
-    })
-    .catch ((err) => {
-        console.log('caught error', err)
-        dayForecastContainer.innerHTML = `caught error: ${err}`
-    }) 
-}
- 
-// Get weather from coordinates
+// Get weather and forecast from coordinates
 const getWeatherFromCoords = (lat, lon) => {
     console.log(lat, lon)
-    const openWeatherCoordApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=`
+    const openWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=`
     console.log('')
-    fetch (openWeatherCoordApiUrl + openWeatherApiKey)
+    fetch (openWeatherUrl + openWeatherApiKey)
     .then(response => response.json())
     .then(json => {
         console.log(json)
-        renderCurrentWeather(json)
+        renderWeather(json)
     })
 }
 
 // Get city and country from coordinates
-const getPlaceFromCoords = () => {
-    if (latitude && longitude) {
+const getPlaceFromCoords = (latitude, longitude) => {
         const url = `https://eu1.locationiq.com/v1/reverse.php?key=${locationIQAccessToken}&lat=${latitude}&lon=${longitude}&format=json`
         fetch(url)
         .then(response => response.json())
         .then(json => {
             console.log(json)
-            city = json.address.city
-            country = json.address.country
+            if (json.address.city) {
+                foundPlace = json.address.city
+            } else if (json.address.county) {
+                foundPlace = json.address.county
+            }
+            console.log(foundPlace)
+            place.innerHTML += `<p>${foundPlace}</p>`
+            // country = json.address.country
             // Call next function
-            getCurrentWeather()
-            getForecast()
+            // getCurrentWeather()
+            // getForecast()
         })
-    } else {
-        console.log('error - no latitude and longitude')
-    }
 }
 
 // Get coordinates from search string
@@ -168,10 +140,11 @@ const getCoordsFromSearch = (str) => {
         console.log(json)
         if (json.length) {
             getWeatherFromCoords(json[0].lat, json[0].lon)
-            getForecastFromCoords(json[0].lat, json[0].lon)
+            getPlaceFromCoords(json[0].lat, json[0].lon)
+            // getForecastFromCoords(json[0].lat, json[0].lon)
 
         } else {
-            currentWeatherContainer = 
+            currentWeather = 
             `<p>Sorry! Didn't find any weather on that location</p>`
         }
     })
@@ -179,12 +152,16 @@ const getCoordsFromSearch = (str) => {
 
 // Get coordinates from Geolocation API
 const getUserCoords = () => {
+    console.log('')
     const success = (position) => {
-        latitude = position.coords.latitude
-        longitude = position.coords.longitude
+        // latitude = position.coords.latitude
+        // longitude = position.coords.longitude
         // Call next function
-        getPlaceFromCoords()
-    }
+        getWeatherFromCoords(position.coords.latitude, position.coords.longitude)
+        getPlaceFromCoords(position.coords.latitude, position.coords.longitude)
+
+        // getForecastFromCoords(json[0].lat, json[0].lon)
+}
     const error = () => {
         console.log('Unable to retrieve your location')
     }
